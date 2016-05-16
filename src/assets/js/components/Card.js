37a7614d9a90a4afe9a74f0c8dfe4d7f67ebf576
Joshua Thornton <joshua.thornton@iqnect.org>
2016-05-16 14:06:19 +0800

    Do not render icon / image if one is not provided.

--- src/assets/js/components/Card.js
+++ src/assets/js/components/Card.js
@@ -2,9 +2,16 @@ import React from 'react';
 import _ from 'lodash';
 
 export default (props) => {
+  let image;
+  if(props.card.icon == null || props.card.icon.length === 0) {
+    image = null;
+  } else {
+    image = <img className="card-img-top img-fluid" src={props.card.icon} alt="Card image cap"/>;
+  }
+
   return (
     <div className="card text-xs-center">
-      <img className="card-img-top img-fluid" src={props.card.icon} alt="Card image cap"/>
+      {image}
       <div className="card-block">
         <h1 className="card-title">
           {props.card.title}
