b5cad2a8a48acc4ad9ce1714530b3372efebb16a
Joshua Thornton <joshua.thornton@iqnect.org>
2016-05-16 14:04:01 +0800

    Aligning pagers.

--- src/assets/js/components/Pager.js
+++ src/assets/js/components/Pager.js
@@ -4,15 +4,15 @@ import classNames from 'classnames';
 
 export default (props) => {
   return (
-    <ul className="pager">
+    <ul className="caro-pager">
       {_.times(props.pageCount, n => {
-        const className = classNames('pager__page', {
-          'pager__page--active': props.activePage === n
+        const className = classNames('caro-pager__page', {
+          'caro-pager__page--active': props.activePage === n
         });
 
         return (
           <li className={className} key={n}>
-            O
+            <div></div>
           </li>
         );
       })}
