be696982eb71b3560118933b68a2df665c58d675
Joshua Thornton <joshua.thornton@iqnect.org>
2016-05-16 13:08:37 +0800

    Using array as initial.

--- src/assets/js/reducers/cards.js
+++ src/assets/js/reducers/cards.js
@@ -1,9 +1,7 @@
 import { createReducer } from 'redux-create-reducer';
 import { FETCH_CARDS_SUCCESS } from '../actions';
 
-export default createReducer({
-
-}, {
+export default createReducer([], {
   [FETCH_CARDS_SUCCESS]: (state, action) => {
     return action.payload.cards;
   }
