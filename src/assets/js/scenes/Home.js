3aefc65b7d3ecbaa817c32c70919dcc5d0fdb728
Joshua Thornton <joshua.thornton@iqnect.org>
2016-05-16 12:18:33 +0800

    Render cards and load cards.

--- src/assets/js/scenes/Home.js
+++ src/assets/js/scenes/Home.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { loadHomeScene } from '../actions';
+import CardList from '../components/CardList';
 
-const render = (props) => {
-  return (
-    <div>
-      Home
-    </div>
-  );
+class Home extends Component {
+
+  componentDidMount() {
+    this.props.actions.load({
+      queryString: document.location.search
+    });
+  }
+
+  render() {
+    return (
+      <div className="scene scene--home container">
+        <div className="col-xs-12">
+          <CardList cards={this.props.cards}/>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default render;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    cards: state.cards
+  };
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    actions: {
+      load: (...args) => {
+        dispatch(loadHomeScene.apply(null, args));
+      }
+    }
+  };
+}
+
+let DecoratedComponent = Home;
+DecoratedComponent = connect(mapStateToProps, mapDispatchToProps)(DecoratedComponent);
+
+export default DecoratedComponent;
